70ce27899f0bcc54573bc8a8d92792e817091bb4
Kimberly <kimolanga123@gmail.com>
2022-06-27 17:30:28 +0300

    update styling

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ var type = new Typed('.typing-text',{
     strings : ['web designer','frontend developer','backend developer','project manager'],
     typespeed: 120,
     loop:true
-})
+});
+
+let themeColor = document.querySelectorAll('.theme-toggler span');
+themeColor.forEach(color => color.addEventListener('click', () =>{
+    let background = color.style.background;
+    document.querySelector('body').style.background = background;
+}));
